e5b1e3d572b78b044f4117fb36765332c034c083
Gustavo Brandão <gugabgta3@gmail.com>
2021-11-22 22:40:40 -0300

    Movido dependencias para serem trackeadas(por enquanto)

--- templates/head.php
+++ templates/head.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../cssDependencies/bootstrap.min.css"/>
-    <link rel="stylesheet" href="../cssDependencies/font-awesome.css"/>
+    <link rel="stylesheet" href="../css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="../css/font-awesome.css"/>
     <link rel="stylesheet" href="../css/main.css"/>
     <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:300,400,700'>
     <title>Easy Events</title>
-    <script src="../jsDependencies/jquery.min.js"></script>
-    <script src="../jsDependencies/all.min.js"></script>
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/all.min.js"></script>
 </head>
 <body>
 <div class="nav-top" style="background-color: <?= backgroundColor ?>;">
